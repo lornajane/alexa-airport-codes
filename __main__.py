5688dcdf6a70c7d9dbb21a9653113ca8bbb58c1b
Lorna Jane Mitchell <lorna@lornajane.net>
2017-12-15 16:51:13 +0000

    Make action useful with or without Alexa

--- __main__.py
+++ __main__.py
@@ -10,13 +10,23 @@ def main(args):
 	password=parsed.password,
 	decode_responses=True)
 
-    code = args['request']['intent']['slots']['Code']['value'].upper()
+    # Alexa request?  Or just look for a code param
+    if 'request' in args:
+        code = args['request']['intent']['slots']['Code']['value'].upper()
+        alexa = true
+    else:
+        code = args['code']
+
     airport1 = redis_client.hgetall("code:" + code)
     airport_info = "Airport code " + code + " is for " + airport1['name'] + " in " + airport1['country']
     print (airport_info);
 
-    speech = {"type": "PlainText", "text": airport_info}
-    response = {"shouldEndSession": "true", "outputSpeech": speech}
-    output = {"version": "1.0", "response": response}
+    if alexa:
+        speech = {"type": "PlainText", "text": airport_info}
+        response = {"shouldEndSession": "true", "outputSpeech": speech}
+        output = {"version": "1.0", "response": response}
+    else:
+        output = airport_info
+
     return output
 
